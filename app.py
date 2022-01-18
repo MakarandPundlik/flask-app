de78cec3e28f62bbcfe44eead2096a85b2e98f51
MakarandPundlik <mmp2308@gmail.com>
2022-01-18 15:54:55 +0530

    lesson 2 completed

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)#reserved keywords 
+
+'''
+duplicate data, just to display the contents 
+'''
+posts = [
+    #list of dictionaries
+    {
+        'author':'makarand',
+        'title':'My fitst blog',
+        'content':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ornare odio rhoncus turpis tincidunt sagittis. Sed eros dui, dapibus nec feugiat sit amet, viverra et leo. Integer hendrerit nibh id facilisis congue. Phasellus augue justo, iaculis at dictum non, egestas et nibh. Suspendisse potenti. Nullam iaculis vulputate ex, id laoreet ligula fermentum eget. Curabitur cursus id erat at tempor. Vestibulum viverra ac felis vulputate egestas. Cras maximus magna sit amet augue tincidunt lobortis.',
+        'date':'10-10-2020'
+    },
+     {
+        'author':'makarand',
+        'title':'second flask blog',
+        'content':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ornare odio rhoncus turpis tincidunt sagittis. Sed eros dui, dapibus nec feugiat sit amet, viverra et leo. Integer hendrerit nibh id facilisis congue. Phasellus augue justo, iaculis at dictum non, egestas et nibh. Suspendisse potenti. Nullam iaculis vulputate ex, id laoreet ligula fermentum eget. Curabitur cursus id erat at tempor. Vestibulum viverra ac felis vulputate egestas. Cras maximus magna sit amet augue tincidunt lobortis.',
+        'date':'11-10-2020'
+    },
+     {
+        'author':'makarand',
+        'title':'third flask blog',
+        'content':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ornare odio rhoncus turpis tincidunt sagittis. Sed eros dui, dapibus nec feugiat sit amet, viverra et leo. Integer hendrerit nibh id facilisis congue. Phasellus augue justo, iaculis at dictum non, egestas et nibh. Suspendisse potenti. Nullam iaculis vulputate ex, id laoreet ligula fermentum eget. Curabitur cursus id erat at tempor. Vestibulum viverra ac felis vulputate egestas. Cras maximus magna sit amet augue tincidunt lobortis.',
+        'date':'12-10-2020'
+    },
+    ]
 
 @app.route("/")
 @app.route("/home") #decorattors 
 def hello_world():
-    return "<h2>Home Page with decorator</h2>"
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about_page():
-    return "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vel quam elementum pulvinar etiam non quam lacus. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci. Vitae congue eu consequat ac felis donec et odio. Feugiat nibh sed pulvinar proin gravida hendrerit lectus. Turpis tincidunt id aliquet risus feugiat in ante metus. Pharetra pharetra massa massa ultricies mi quis hendrerit. Tristique nulla aliquet enim tortor at. Sit amet nisl suscipit adipiscing bibendum est ultricies integer. Id faucibus nisl tincidunt eget. Malesuada nunc vel risus commodo. Porta nibh venenatis cras sed. Scelerisque eleifend donec pretium vulputate.</p>"
+    return render_template('about.html',title="About Page")
 
 if __name__ == '__main__': #treats file as independent python program 
     app.run(debug=True)     
